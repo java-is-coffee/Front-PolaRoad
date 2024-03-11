1472415e99df80d3fd57e59a71c142e10bb2f285
koreaygj <ygj1828@gmail.com>
2024-03-11 13:44:13 +0900

    Add : 기본 Router 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./routes/home";
+import Explore from "./routes/explore";
+import MyPage from "./routes/myPage";
 
 function App() {
-  return <div className="App">start project</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/explore" element={<Explore />}></Route>
+          <Route path="/my" element={<MyPage />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
