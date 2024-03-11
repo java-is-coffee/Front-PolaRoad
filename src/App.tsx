337fb3ab9d5b0a30b62f6b5e01710d68a7a5ecf4
Noh <nohsa97@nate.com>
2024-03-11 12:03:56 +0900

    Feat : PO-13 로그인 라우터 설정
    - 기초 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/login/login";
 
 function App() {
-  return <div className="App">start project</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
