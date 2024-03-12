9106a00feb4652f8ebc2ca2f3601b11af6fc86c4
koreaygj <ygj1828@gmail.com>
2024-03-12 21:33:47 +0900

    Remove : 컨벤션 이전 버전 삭제

--- src/containers/explore/exploreContainer.tsx
+++
@@ -1,7 +0,0 @@
-import exploreContainerStyles from "./ExploreContainer.module.css";
-
-function ExploreContainer() {
-  return <div className={exploreContainerStyles.wrapper}></div>;
-}
-
-export default ExploreContainer;
