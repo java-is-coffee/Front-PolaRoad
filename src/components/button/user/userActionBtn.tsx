48accd799ed4405b56440fb27870ce014373b61b
koreaygj <ygj1828@gmail.com>
2024-03-12 20:04:02 +0900

    Feat : PO-20 마이페이지 유저 액션 버튼 컴포넌트 구현 새로운 포스트, 새로운 앨범 버튼 적용

---
+++ src/components/button/user/userActionBtn.tsx
@@ -0,0 +1,16 @@
+import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
+import btnStyle from "./userActionBtn.module.css";
+interface userActionBtnProps {
+  name: string;
+  icon: ReactJSXElement;
+}
+function UserActionBtn({ name, icon }: userActionBtnProps) {
+  return (
+    <button className={btnStyle.btn}>
+      {icon}
+      {name}
+    </button>
+  );
+}
+
+export default UserActionBtn;
