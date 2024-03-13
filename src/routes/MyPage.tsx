0ac6e94b0070ea59a8ea12dbd6a8da9c2bc21e55
koreaygj <ygj1828@gmail.com>
2024-03-14 00:17:27 +0900

    Feat : PO-17 마이페이지 new post 모달 등록

--- src/routes/MyPage.tsx
+++ src/routes/MyPage.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
 import Header from "../components/header/Header";
 import MyPageContainer from "../containers/myPage/MyPageContainer";
+import ModalOption from "../enum/modalOptionTypes";
+import { useModal } from "../hooks/modal/ModalProvider";
+import NewPostModal from "../components/modal/newPost/NewPostModal";
 
 function MyPage() {
+  const { registerModal, closeModal } = useModal();
+  useEffect(() => {
+    registerModal(ModalOption.POST, <NewPostModal />);
+    return () => closeModal(ModalOption.POST);
+  }, []);
   return (
     <div>
       <Header />
