60bbc1c6481b676e8d53d120d0bded2be09d9785
koreaygj <ygj1828@gmail.com>
2024-03-14 00:18:38 +0900

    Feat : PO-17 new post 버튼 핸들러 구성

--- src/components/button/user/UserActionBtn.tsx
+++ src/components/button/user/UserActionBtn.tsx
@@ -1,12 +1,21 @@
 import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
 import btnStyle from "./UserActionBtn.module.css";
+import ModalOption from "../../../enum/modalOptionTypes";
+import { useModal } from "../../../hooks/modal/ModalProvider";
 interface userActionBtnProps {
   name: string;
   icon: ReactJSXElement;
+  type: ModalOption;
 }
-function UserActionBtn({ name, icon }: userActionBtnProps) {
+function UserActionBtn({ name, icon, type }: userActionBtnProps) {
+  const { openModal } = useModal();
+  const handleModal = () => {
+    openModal(type);
+    console.log("open modal");
+    console.log(type);
+  };
   return (
-    <button className={btnStyle.btn}>
+    <button className={btnStyle.btn} onClick={handleModal}>
       {icon}
       {name}
     </button>
