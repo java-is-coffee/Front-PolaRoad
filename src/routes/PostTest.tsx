27be9ac438ce946fc1c68e5927db2ff4d2c0642b
koreaygj <ygj1828@gmail.com>
2024-03-14 15:36:33 +0900

    Temp : PO-17 temp test page 구현

--- src/routes/PostTest.tsx
+++ src/routes/PostTest.tsx
@@ -1,15 +1,47 @@
 import { useState } from "react";
 import putFile from "../aws/putFile";
+import useBucket from "../hooks/bucket/useBucket";
+import { IUploadImage } from "../interface/bucket/IUploadImage";
+
+const IMAGE_URL = process.env.REACT_APP_BUCKET_BASEURL;
 
 function PostTest() {
   const [input, setInput] = useState<File | null>();
+  const { uploadImage, deleteImage } = useBucket();
   const handleUpload = () => {
     if (input) {
-      console.log(input);
-      putFile(input); // 파일 업로드 함수 호출
+      const data: IUploadImage = {
+        postUserId: 1, // Ensure these IDs are of the correct type (string vs number)
+        postId: 1, // Adjust if necessary
+        image: input,
+      };
+
+      (async () => {
+        try {
+          const uploadPath = await uploadImage({
+            type: "POST",
+            imageInfo: data,
+          });
+          console.log(uploadPath);
+        } catch (error) {
+          console.error("Upload failed", error);
+        }
+      })();
     } else {
       console.log("No file selected");
     }
+  };
+  const handleDelate = () => {
+    (async () => {
+      try {
+        const result = await deleteImage(
+          "1/post/1/788a5add-a437-a233-bfed-bdd46a2d8aedIMG_5878.jpeg"
+        );
+        console.log(result);
+      } catch (error) {
+        console.error("Upload failed", error);
+      }
+    })();
   };
   const handleFile = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
@@ -18,10 +50,12 @@ function PostTest() {
   };
   return (
     <div>
-      <input id="files" type="file" onChange={handleFile} />
+      <input id="files" type="file" onChange={handleFile} required />
+      <img src={`${IMAGE_URL}/docker.png`} />
       <button id="button" onClick={handleUpload}>
         올리기
       </button>
+      <button onClick={handleDelate}>삭제하기</button>
     </div>
   );
 }
