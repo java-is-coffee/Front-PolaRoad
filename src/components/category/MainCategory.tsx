bebccaa940dd2e0c993b9b032b0a147e0cd56e33
Noh <nohsa97@nate.com>
2024-03-15 02:54:03 +0900

    Feat : PO-15 탐색 페이지 수정
    - 헤더 부분으로 검색 창 이동.
    - 탐색 페이지 카테고리 필터 클릭 시, 모달 창 팝업
    - 리덕스 적용을 통한 카테고리 & 필터 설정 구현 예정 (03.16)

--- src/components/category/MainCategory.tsx
+++ src/components/category/MainCategory.tsx
@@ -7,67 +7,88 @@ import DirectionsWalkIcon from "@mui/icons-material/DirectionsWalk";
 import TrainIcon from "@mui/icons-material/Train";
 import DirectionsCarFilledIcon from "@mui/icons-material/DirectionsCarFilled";
 import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
+import TuneIcon from "@mui/icons-material/Tune";
+import { useModal } from "../../hooks/modal/ModalProvider";
+import ModalOption from "../../enum/modalOptionTypes";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store/store";
 // import { useState } from "react";
 // import CategoryType from "../../enum/categoryOptionType";
 
 const MainCategory = () => {
   //   const [setCategory, setSetCategory] = useState(CategoryType.NULL);
 
-  return (
-    <div className={styles.categoryContainer}>
-      <label
-        id="HOT"
-        className={`${styles.categoryLabel}`}
-        // onClick={() => setSetCategory(CategoryType.HOT)}
-      >
-        <div className={styles.categoryItem}>
-          <StarIcon className={styles.categoryIcon} />
-          인기 게시글
-        </div>
-      </label>
-      <label className={styles.categoryLabel}>
-        <div className={styles.categoryItem}>
-          <KebabDiningIcon className={styles.categoryIcon} />
-          식도락
-        </div>
-      </label>
-      <label className={styles.categoryLabel}>
-        <div className={styles.categoryItem}>
-          <ApartmentIcon className={styles.categoryIcon} />
-          도시 경관
-        </div>
-      </label>
-      <label className={styles.categoryLabel}>
-        <div className={styles.categoryItem}>
-          <ForestIcon className={styles.categoryIcon} />
-          자연
-        </div>
-      </label>
+  const activeCategory = useSelector(
+    (state: RootState) => state.setCategory.activeCategory
+  );
 
-      <label className={styles.categoryLabel}>
-        <div className={styles.categoryItem}>
-          <DirectionsWalkIcon className={styles.categoryIcon} />
-          도보 여행
-        </div>
-      </label>
-      <label className={styles.categoryLabel}>
-        <div className={styles.categoryItem}>
-          <TrainIcon className={styles.categoryIcon} />
-          기차 여행
-        </div>
-      </label>
-      <label className={styles.categoryLabel}>
-        <div className={styles.categoryItem}>
-          <DirectionsCarFilledIcon className={styles.categoryIcon} />
-          자동차 여행
-        </div>
-      </label>
-      <label className={styles.categoryLabel}>
-        <div className={styles.categoryItem}>
-          <PhotoCameraIcon className={styles.categoryIcon} />
-          포토 스팟
-        </div>
-      </label>
+  const { openModal } = useModal();
+
+  const showFilterModal = () => {
+    openModal(ModalOption.SEARCH);
+  };
+
+  return (
+    <div className={styles.MainCategoryTap}>
+      <div className={styles.categoryContainer}>
+        <label
+          id="HOT"
+          className={`${styles.categoryLabel}`}
+          // onClick={() => setSetCategory(CategoryType.HOT)}
+        >
+          <div className={styles.categoryItem}>
+            <StarIcon className={styles.categoryIcon} />
+            인기 게시글
+          </div>
+        </label>
+        <label className={styles.categoryLabel}>
+          <div className={styles.categoryItem}>
+            <KebabDiningIcon className={styles.categoryIcon} />
+            식도락
+          </div>
+        </label>
+        <label className={styles.categoryLabel}>
+          <div className={styles.categoryItem}>
+            <ApartmentIcon className={styles.categoryIcon} />
+            도시 경관
+          </div>
+        </label>
+        <label className={styles.categoryLabel}>
+          <div className={styles.categoryItem}>
+            <ForestIcon className={styles.categoryIcon} />
+            자연
+          </div>
+        </label>
+
+        <label className={styles.categoryLabel}>
+          <div className={styles.categoryItem}>
+            <DirectionsWalkIcon className={styles.categoryIcon} />
+            도보 여행
+          </div>
+        </label>
+        <label className={styles.categoryLabel}>
+          <div className={styles.categoryItem}>
+            <TrainIcon className={styles.categoryIcon} />
+            기차 여행
+          </div>
+        </label>
+        <label className={styles.categoryLabel}>
+          <div className={styles.categoryItem}>
+            <DirectionsCarFilledIcon className={styles.categoryIcon} />
+            자동차 여행
+          </div>
+        </label>
+        <label className={styles.categoryLabel}>
+          <div className={styles.categoryItem}>
+            <PhotoCameraIcon className={styles.categoryIcon} />
+            포토 스팟
+          </div>
+        </label>
+      </div>
+      <div className={styles.filterButton} onClick={showFilterModal}>
+        <TuneIcon sx={{ fontSize: "2rem", marginRight: "0.5rem" }} />
+        필터
+      </div>
     </div>
   );
 };
--- src/components/dropDown/search/SearchDropdown.tsx
+++ src/components/dropDown/search/SearchDropdown.tsx
@@ -1,6 +1,31 @@
+import { useEffect } from "react";
+import ModalOption from "../../../enum/modalOptionTypes";
+import { useModal } from "../../../hooks/modal/ModalProvider";
 import dropdownStyles from "./SearchDropdown.module.css";
 
 function SearchDropdown() {
+  const { closeModal } = useModal();
+  // const FormComponent = formComponents[postFormIndex];
+  // Esc 눌렀을때 모달 탈출
+  const handleKeyUp = (event: KeyboardEvent) => {
+    if (event.key === "Escape") {
+      closeModal(ModalOption.SEARCH);
+    }
+  };
+  // 모달 바깥쪽 눌렀을떄 모달 탈출
+  const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      closeModal(ModalOption.SEARCH);
+    }
+  };
+  // 컴포넌트 랜더링시에 한번만 리스너 추가
+  useEffect(() => {
+    document.addEventListener("keyup", handleKeyUp);
+    return () => {
+      document.removeEventListener("keyup", handleKeyUp);
+    };
+    // eslint-disable-next-line
+  }, []);
   const category: string[] = [
     "인기게시글",
     "식도락",
@@ -31,51 +56,53 @@ function SearchDropdown() {
   ];
 
   return (
-    <div className={dropdownStyles.wrapper}>
-      {/* 정렬 및 첫 줄 */}
-      <div className={dropdownStyles.head}>
-        필터
-        <span>X</span>
-      </div>
-      <div className={dropdownStyles.line}></div>
+    <div className={dropdownStyles.outSide} onClick={handleBackdropClick}>
+      <div className={dropdownStyles.wrapper}>
+        {/* 정렬 및 첫 줄 */}
+        <div className={dropdownStyles.head}>
+          필터
+          <span>X</span>
+        </div>
+        <div className={dropdownStyles.line}></div>
 
-      {/* 정렬  */}
-      <div className={dropdownStyles.option}>
-        <div className={dropdownStyles.title}>정렬</div>
-        <div className={dropdownStyles.list}>
-          <div className={dropdownStyles.item}>
-            <span>최신순</span>
-          </div>
-          <div className={dropdownStyles.item}>
-            <span>인기순</span>
+        {/* 정렬  */}
+        <div className={dropdownStyles.option}>
+          <div className={dropdownStyles.title}>정렬</div>
+          <div className={dropdownStyles.list}>
+            <div className={dropdownStyles.item}>
+              <span>최신순</span>
+            </div>
+            <div className={dropdownStyles.item}>
+              <span>인기순</span>
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* 구분선 */}
-      <div className={dropdownStyles.line}></div>
+        {/* 구분선 */}
+        <div className={dropdownStyles.line}></div>
 
-      <div className={dropdownStyles.option}>
-        <div className={dropdownStyles.title}>카테고리</div>
-        <div className={dropdownStyles.list}>
-          {category.map((item) => (
-            <div className={dropdownStyles.item}>
-              <span>{item}</span>
-            </div>
-          ))}
+        <div className={dropdownStyles.option}>
+          <div className={dropdownStyles.title}>카테고리</div>
+          <div className={dropdownStyles.list}>
+            {category.map((item) => (
+              <div className={dropdownStyles.item} key={item}>
+                <span>{item}</span>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
 
-      {/* 구분선 */}
-      <div className={dropdownStyles.line}></div>
-      <div className={dropdownStyles.option}>
-        <div className={dropdownStyles.title}>지역</div>
-        <div className={dropdownStyles.list}>
-          {local.map((item) => (
-            <div className={dropdownStyles.item}>
-              <span>{item}</span>
-            </div>
-          ))}
+        {/* 구분선 */}
+        <div className={dropdownStyles.line}></div>
+        <div className={dropdownStyles.option}>
+          <div className={dropdownStyles.title}>지역</div>
+          <div className={dropdownStyles.list}>
+            {local.map((item) => (
+              <div className={dropdownStyles.item} key={item}>
+                <span>{item}</span>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
