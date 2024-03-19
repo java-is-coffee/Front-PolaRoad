8d02016d1df824302567b494d0b6ba9b50a99c4b
koreaygj <ygj1828@gmail.com>
2024-03-19 18:17:50 +0900

    Feat : PO-17 폼 Step 적용

--- src/components/modal/newPost/NewPostModal.tsx
+++ src/components/modal/newPost/NewPostModal.tsx
@@ -6,12 +6,29 @@ import NewPostTheme from "../../form/post/NewPostTheme";
 import NewCardList from "../../form/card/NewCardList";
 import { useModal } from "hooks/modal/ModalProvider";
 import ModalOption from "enum/modalOptionTypes";
+import { Step, StepLabel, Stepper } from "@mui/material";
+import { useSelector } from "react-redux";
+import { RootState } from "redux/store/store";
+import INewPost from "interface/post/INewPost";
+import { toast } from "react-toastify";
 
-const formComponents = [NewPostTheme, NewCardList, NewPostDetails];
+interface FormComponentsType {
+  name: string;
+  component: React.ElementType;
+}
+
+const formComponents: FormComponentsType[] = [
+  { name: "테마 선정", component: NewPostTheme },
+  { name: "카드 리스트", component: NewCardList },
+  { name: "포스트 상세", component: NewPostDetails },
+];
 
 function NewPostModal() {
   const { openModal } = useModal();
   const [postFormIndex, setPostFormIndex] = useState<number>(0);
+  const postDetails: INewPost = useSelector(
+    (state: RootState) => state.newPost
+  );
   const modalRef = useRef<HTMLDivElement>(null); // 모달 DOM에 접근하기 위한 ref
 
   // useEffect(() => {
@@ -52,7 +69,21 @@ function NewPostModal() {
 
   const increaseFormIndex = () => {
     if (postFormIndex < formComponents.length - 1) {
-      setPostFormIndex((prev) => prev + 1);
+      if (postFormIndex === 0) {
+        if (postDetails.concept && postDetails.region) {
+          setPostFormIndex((prev) => prev + 1);
+        } else {
+          toast.error("theme 와 region은 필수 항목입니다.");
+        }
+      } else if (postFormIndex === 1) {
+        if (postDetails.cards) {
+          setPostFormIndex((prev) => prev + 1);
+        } else {
+          toast.error("최소 하나의 카드를 입력해주세요");
+        }
+      } else {
+        setPostFormIndex((prev) => prev + 1);
+      }
     }
   };
 
@@ -85,7 +116,14 @@ function NewPostModal() {
               <ActionBtn name="업로드" handleClick={handleUploadPost} />
             )}
           </div>
-          {formComponents.map((Component, index) => (
+          <Stepper alternativeLabel activeStep={postFormIndex}>
+            {formComponents.map((Component, index) => (
+              <Step key={index}>
+                <StepLabel>{Component.name}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          {formComponents.map(({ component: Component }, index) => (
             <div
               key={index}
               style={{ display: index === postFormIndex ? "block" : "none" }}
