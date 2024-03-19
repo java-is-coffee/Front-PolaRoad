8b8d811e6a7ce0bd9a9f7d12d0dba276d90da879
koreaygj <ygj1828@gmail.com>
2024-03-19 18:15:33 +0900

    Refactor : PO-17 카카오 훅 ref 적용

--- src/hooks/map/useKakaoMap.ts
+++ src/hooks/map/useKakaoMap.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 declare global {
   interface Window {
@@ -6,73 +6,74 @@ declare global {
   }
 }
 
-const kakao = (window as any).kakao;
+const kakao = window.kakao;
+
+interface Place {
+  place_name: string;
+  y: string; // latitude
+  x: string; // longitude
+  // 필요에 따라 추가 프로퍼티 선언
+}
 
 interface KakaoMapProps {
   latitude: number;
   longitude: number;
+  level: number;
 }
 
-const useKakaoMap = ({ latitude, longitude }: KakaoMapProps) => {
-  const [map, setMap] = useState<any | null>(null);
-  const [selectedPlace, setSelectedPlace] = useState();
-  const infoWindow = new kakao.maps.InfoWindow(
-    { zIndex: 1 },
-    { passive: true }
-  );
+const useKakaoMap = ({ latitude, longitude, level }: KakaoMapProps) => {
+  const mapRef = useRef<any>(null); // map 인스턴스를 저장하기 위한 ref
+  const infoWindowRef = useRef<any>(null); // infoWindow 인스턴스를 저장하기 위한 ref
+  const [selectedPlace, setSelectedPlace] = useState<Place>();
 
-  // 지도 옵션 정의
-  const mapOption = {
-    center: new kakao.maps.LatLng(latitude, longitude),
-    level: 3,
-  };
-
-  // 지도 초기화 함수
   const initKakaoMap = (mapContainer: HTMLElement) => {
+    if (mapRef.current && infoWindowRef.current) return;
+    const mapOption = {
+      center: new kakao.maps.LatLng(latitude, longitude),
+      level: level,
+    };
     const newMap = new kakao.maps.Map(mapContainer, mapOption);
-    setMap(newMap);
+    const newInfoWindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+    mapRef.current = newMap;
+    infoWindowRef.current = newInfoWindow;
   };
 
   // 키워드 검색 결과 처리 함수
-  const searchResult = (data: any, status: any) => {
-    if (status === kakao.maps.services.Status.OK && map) {
+  const searchResult = (data: Place[], status: any) => {
+    if (status === kakao.maps.services.Status.OK && mapRef.current) {
       const bounds = new kakao.maps.LatLngBounds();
-      data.forEach((place: any) => {
+      data.forEach((place) => {
         displayMarker(place);
         bounds.extend(new kakao.maps.LatLng(place.y, place.x));
       });
-      map.setBounds(bounds);
+      mapRef.current.setBounds(bounds);
     }
   };
 
   // 키워드로 장소를 검색하는 함수
   const searchPlaceByKeyword = (keyword: string) => {
-    if (!map) return;
-    const places = new kakao.maps.services.Places(map);
+    if (!mapRef.current) return;
+    const places = new kakao.maps.services.Places();
+    places.setMap(mapRef);
+    console.log(places.set);
     places.keywordSearch(keyword, searchResult);
   };
 
   // 지도에 마커를 표시하는 함수
-  const displayMarker = (place: any) => {
-    if (!map) return;
+  const displayMarker = (place: Place) => {
+    if (!mapRef.current || !infoWindowRef.current) return;
 
     const marker = new kakao.maps.Marker({
-      map: map,
+      map: mapRef.current,
       position: new kakao.maps.LatLng(place.y, place.x),
     });
-
-    kakao.maps.addEventListener(
-      marker,
-      "click",
-      () => {
-        infoWindow.setContent(
-          `<div style="padding:5px;font-size:12px;">${place.place_name}</div>`
-        );
-        infoWindow.open(map, marker);
-        setSelectedPlace(place);
-      },
-      { passive: true }
-    );
+    kakao.maps.event.addListener(marker, "click", () => {
+      infoWindowRef.current.setContent(
+        `<div style="padding:5px;font-size:12px;">${place.place_name}</div>`
+      );
+      infoWindowRef.current.open(mapRef.current, marker);
+      setSelectedPlace(place);
+    });
   };
 
   return { selectedPlace, initKakaoMap, searchPlaceByKeyword };
