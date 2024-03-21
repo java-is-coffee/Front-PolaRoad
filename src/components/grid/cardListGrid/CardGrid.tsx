30fec3e3c4ee83800e0c0ee88e588a2a39f858e6
koreaygj <ygj1828@gmail.com>
2024-03-21 15:50:01 +0900

    Styles : PO-17 그리드 스타일 적용

--- src/components/grid/cardListGrid/CardGrid.tsx
+++ src/components/grid/cardListGrid/CardGrid.tsx
@@ -1,5 +1,7 @@
 // CardGrid.tsx
 import INewCard from "interface/card/INewCard";
+import gridStyles from "./CardGrid.module.css";
+import { IoMdCloseCircle } from "react-icons/io";
 import React from "react";
 
 interface CardProps {
@@ -7,32 +9,18 @@ interface CardProps {
 }
 
 const CardGrid = ({ cards }: CardProps) => {
-  const sectionStyle = {
-    width: "400px",
-    display: "grid",
-    gridTemplateColumns: "repeat(auto-fill, minmax(100px, 1fr))",
-    gap: "10px",
-    marginBottom: "20px",
-  };
-
-  const imgStyle = {
-    width: "100%",
-    height: "auto",
-    aspectRatio: "1 / 1",
-    borderRadius: "8px",
-    boxShadow: "0 2px 5px rgba(0,0,0,0.2)",
-  };
-
   return (
-    <section style={sectionStyle}>
+    <section className={gridStyles.section}>
       {cards.map((card, index) => (
-        <img
-          key={index}
-          id={`card-${index}`}
-          style={imgStyle}
-          src={`${process.env.REACT_APP_BUCKET_BASEURL}/${card.image}`}
-          alt={`Card ${index}`}
-        />
+        <div key={index} className={gridStyles.card}>
+          <IoMdCloseCircle className={gridStyles.deleteBtn} size={"20px"} />
+          <img
+            id={`card-${index}`}
+            className={gridStyles.img}
+            src={`${process.env.REACT_APP_BUCKET_BASEURL}/${card.image}`}
+            alt={`Card ${index}`}
+          />
+        </div>
       ))}
     </section>
   );
