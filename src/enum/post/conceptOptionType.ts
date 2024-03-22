eadc900da9c469f12701323950a2f5e2909d155a
koreaygj <ygj1828@gmail.com>
2024-03-22 10:25:04 +0900

    Chore : PO-17 컨셉 enum 종류 수정

--- src/enum/post/conceptOptionType.ts
+++ src/enum/post/conceptOptionType.ts
@@ -1,15 +1,15 @@
 enum conceptOptionType {
   FOOD = "음식",
-  CAFE = "카페",
   NATURE = "자연",
   CITY = "도시관광",
   PHOTO = "사진&명소",
   HOT = "인기게시글",
   WALK = "도보여행",
   CAR = "자동차",
-  BUS = "버스",
   TRAIN = "기차여행",
-  AIRPLANE = "비행기여행",
+  // CAFE = "카페",
+  // BUS = "버스",
+  // AIRPLANE = "비행기여행",
 }
 
 export default conceptOptionType;
