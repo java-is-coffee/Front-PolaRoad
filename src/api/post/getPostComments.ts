1b2474f2a9c8535efd0d896680b063def2fbcdde
koreaygj <ygj1828@gmail.com>
2024-03-25 18:22:34 +0900

    Feat : PO-82 Post comment 불러오기 메서드 구현

---
+++ src/api/post/getPostComments.ts
@@ -0,0 +1,24 @@
+import { ICommentDTO } from "./../../interface/comments/ICommentsDTO";
+import { axiosInstance } from "api/token/axiosInstance";
+
+const getPostComments = async (postId: string): Promise<ICommentDTO | null> => {
+  try {
+    const API_URL = `/api/review/post/${postId}`;
+
+    const response = await axiosInstance.get(API_URL);
+
+    const { status, data } = response;
+    console.log(data);
+    if (status === 200) {
+      console.log("postDetails fetch success");
+      return data as ICommentDTO;
+    }
+    return null;
+  } catch (error) {
+    console.log("postDetails fetch fail");
+    console.error(error);
+    return null;
+  }
+};
+
+export default getPostComments;
