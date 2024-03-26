4e77795f4a761a8a69998177e37b1636d6e6611f
koreaygj <ygj1828@gmail.com>
2024-03-26 15:29:00 +0900

    Feat : PO-75 Comment DTO 구현

---
+++ src/interface/comments/ICommentsDTO.ts
@@ -0,0 +1,15 @@
+export interface ICommentDTO {
+  reviewId: number; // 리뷰 Id
+  postId: number; // 포스트 Id
+  memberId: number; // 멤버 Id
+  profileImage: string; // 프로필 이미지
+  nickname: string; // 멤버 닉네임
+  content: string; // 내용
+  reviewPhotoInfoList: ReviewPhotoInfoDto[]; // 리뷰 사진 정보 리스트
+  updatedTime: string; // 업데이트된 시간
+}
+
+interface ReviewPhotoInfoDto {
+  reviewPhotoId: number; // 리뷰 사진 Id
+  reviewPhotoUrl: string; // 리뷰 사진 URL
+}
