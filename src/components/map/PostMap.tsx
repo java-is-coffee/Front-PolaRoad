a6c5c1ab6bb8ee8d26e7034b89db86dd05e68a6d
koreaygj <ygj1828@gmail.com>
2024-03-29 18:35:48 +0900

    Feat : PO-79 맵 컴포넌트 추가

--- src/components/map/PostMap.tsx
+++ src/components/map/PostMap.tsx
@@ -2,25 +2,20 @@ import useKakaoMap from "hooks/map/useKakaoMap";
 import mapStyles from "./PostMap.module.css";
 import { useEffect, useRef, useState } from "react";
 import INewCard from "interface/card/INewCard";
+import { IRoutesPointType } from "interface/map/IRoutesPointType";
 
 interface PostMapProps {
   cards: INewCard[];
 }
 
-interface RoutesPointType {
-  latitude: number | null;
-  longitude: number | null;
-}
-
 function PostMap({ cards }: PostMapProps) {
-  const [routesPoints, setRoutesPoints] = useState<RoutesPointType[]>([]);
-  const { initKakaoMap, getLatLng } = useKakaoMap();
+  const [routesPoints, setRoutesPoints] = useState<IRoutesPointType[]>([]);
+  const { initKakaoMap, calculateCenterPoint, renderOverlay } = useKakaoMap();
   const containerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     if (!containerRef.current) return;
-    console.log(cards);
-    const routePointData: RoutesPointType[] = cards
+    const routePointData: IRoutesPointType[] = cards
       .filter(
         (card) =>
           Number.isFinite(card.latitude) && Number.isFinite(card.longitude)
@@ -30,29 +25,9 @@ function PostMap({ cards }: PostMapProps) {
         longitude: card.longitude,
       }));
     setRoutesPoints(routePointData);
-    console.log(routePointData);
-
-    const calculateCenterPoint = () => {
-      if (routePointData.length === 0) return { lat: 0, lng: 0 };
-
-      const sum = routePointData.reduce(
-        (acc, curr) => {
-          acc.lat += curr.latitude ? curr.latitude : 0;
-          acc.lng += curr.longitude ? curr.longitude : 0;
-          return acc;
-        },
-        { lat: 0, lng: 0 }
-      );
-
-      return {
-        lat: sum.lat / routePointData.length,
-        lng: sum.lng / routePointData.length,
-      };
-    };
-
-    const centerPoint = calculateCenterPoint();
-    console.log(getLatLng(centerPoint.lng, centerPoint.lat));
+    const centerPoint = calculateCenterPoint(routePointData);
     initKakaoMap(containerRef.current, centerPoint.lat, centerPoint.lng, 1);
+    renderOverlay(routePointData);
   }, [containerRef.current]);
   return (
     <div className={mapStyles.wrapper}>
