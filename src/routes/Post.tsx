531806fb1a9c3e6962cb670b4124db31a4689390
koreaygj <ygj1828@gmail.com>
2024-03-31 19:26:55 +0900

    Feat : PO-75 포스트 옵션 모달 적용

--- src/routes/Post.tsx
+++ src/routes/Post.tsx
@@ -1,7 +1,18 @@
 import Header from "components/header/Header";
+import PostOptionModal from "components/modal/option/PostOptionModal";
 import PostDetail from "containers/post/PostDetails";
+import ModalOption from "enum/modalOptionTypes";
+import { useModal } from "hooks/modal/ModalProvider";
+import { useEffect } from "react";
 
 function Post() {
+  const { registerModal, closeModal } = useModal();
+  useEffect(() => {
+    registerModal(ModalOption.POSTOPTION, <PostOptionModal />);
+    return () => {};
+    closeModal(ModalOption.POSTOPTION);
+    // eslint-disable-next-line
+  }, []);
   return (
     <div>
       <Header />
