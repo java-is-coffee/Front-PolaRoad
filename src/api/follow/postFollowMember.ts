e7c68d867dcc0da03c12798435c8ccc8c1b72628
koreaygj <ygj1828@gmail.com>
2024-03-31 19:54:40 +0900

    Feat : PO-75 팔로우 기능 추가

---
+++ src/api/follow/postFollowMember.ts
@@ -0,0 +1,25 @@
+import getMemberInfo from "api/member/getMemberInfo";
+import { axiosInstance } from "api/token/axiosInstance";
+import { toast } from "react-toastify";
+
+const postFollowMember = async (
+  followingMemberId: number
+): Promise<boolean> => {
+  try {
+    const API_URL = `/api/member/my/follow/${followingMemberId}`;
+
+    const response = await axiosInstance.post(API_URL);
+
+    const { status, data } = response;
+    if (status === 200) {
+      toast.info("팔로우 완료");
+      return true;
+    }
+    return false;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
+
+export default postFollowMember;
