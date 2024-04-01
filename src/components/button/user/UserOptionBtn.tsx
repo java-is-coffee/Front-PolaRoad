fc3983b107b48823bd1eff97463ce5d4532efbe0
koreaygj <ygj1828@gmail.com>
2024-04-01 14:51:10 +0900

    Feat : POo-20 프로필 이미지 변경 모달 연결

--- src/components/button/user/UserOptionBtn.tsx
+++ src/components/button/user/UserOptionBtn.tsx
@@ -2,9 +2,14 @@ import btnStyle from "./UserOptionBtn.module.css";
 
 interface userOptionProps {
   name: string;
+  clickAction?: () => void;
 }
-function UserOptionBtn({ name }: userOptionProps) {
-  return <button className={btnStyle.btn}>{name}</button>;
+function UserOptionBtn({ name, clickAction }: userOptionProps) {
+  return (
+    <button className={btnStyle.btn} onClick={clickAction}>
+      {name}
+    </button>
+  );
 }
 
 export default UserOptionBtn;
--- src/containers/profile/ProfileCard.tsx
+++ src/containers/profile/ProfileCard.tsx
@@ -7,22 +7,52 @@ import { CgFileAdd } from "react-icons/cg";
 import { MdOutlinePhotoAlbum } from "react-icons/md";
 import ModalOption from "../../enum/modalOptionTypes";
 import { IMemberInfoDetails } from "interface/member/IMemberInfoDetails";
+import useBucket from "hooks/bucket/useBucket";
+import { useEffect, useState } from "react";
+import { useModal } from "hooks/modal/ModalProvider";
 
 interface ProfileCardProps {
   memberInfo: IMemberInfoDetails;
 }
 
 function ProfileCard({ memberInfo }: ProfileCardProps) {
+  const [profileImgURL, setProfileImgURL] = useState<string>("");
+  const { getImage } = useBucket();
+  const { openModal } = useModal();
+
+  useEffect(() => {
+    const fetchProfileImg = async () => {
+      const imgUrl = await getImage(memberInfo.profileImage);
+      if (imgUrl) setProfileImgURL(imgUrl);
+    };
+    fetchProfileImg();
+    // eslint-disable-next-line
+  }, []);
+
+  const handleEditProfileImg = () => {
+    openModal(ModalOption.EDITPROFILEIMG);
+  };
+
   return (
     <div className={profileStyles.card}>
-      <div className={profileStyles.imgContainer}>
-        <ProfileImg size={"200px"} />
+      <div
+        className={profileStyles.imgContainer}
+        onClick={handleEditProfileImg}
+      >
+        {memberInfo.profileImage ? (
+          <img src={profileImgURL} alt="profile"></img>
+        ) : (
+          <ProfileImg size={"200px"} />
+        )}
       </div>
       <div className={profileStyles.header}>
         <span>{memberInfo.nickname}</span>
         <div className={profileStyles.action}>
           <UserOptionBtn name="앨범보기" />
-          <UserOptionBtn name="프로필 변경" />
+          <UserOptionBtn
+            name="프로필 변경"
+            clickAction={handleEditProfileImg}
+          />
           <IoIosSettings size={"24px"} />
         </div>
       </div>
