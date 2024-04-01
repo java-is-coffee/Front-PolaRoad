885897e33d1e256ddfc2f6e300f5847d3639913e
koreaygj <ygj1828@gmail.com>
2024-04-01 14:19:15 +0900

    Feat : PO-20 로그인시 이전에 페이지가 없을 경우 explore로 리다이렉트

--- src/hooks/login/useLogin.ts
+++ src/hooks/login/useLogin.ts
@@ -1,10 +1,11 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import postLogin, { LoginDTO, LoginData } from "../../api/login/postLogin";
 import { toast } from "react-toastify";
 import secureLocalStorage from "react-secure-storage";
 
 const useLogin = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const Login = async (inputData: LoginData) => {
     const inputDTO: LoginDTO = {
@@ -14,7 +15,8 @@ const useLogin = () => {
     const result = await postLogin(inputDTO);
 
     if (result === 200) {
-      navigate(-1);
+      const from = location.state?.from?.pathname || "/explore"; // 이전 페이지가 없다면 '/explore'로 설정
+      navigate(from);
     } else {
       toast.error("로그인 실패");
     }
