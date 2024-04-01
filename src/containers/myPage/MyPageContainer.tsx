21bc381bbbd42046bb113b64b15aaf37735bbe07
koreaygj <ygj1828@gmail.com>
2024-04-01 13:48:33 +0900

    Feat : PO-20 유저정보 가져와서 렌더링 구현

--- src/containers/myPage/MyPageContainer.tsx
+++ src/containers/myPage/MyPageContainer.tsx
@@ -1,13 +1,33 @@
+import { useEffect, useState } from "react";
 import ProfileCard from "../profile/ProfileCard";
 import UserHistoryContainer from "../userGallery/UserGallery";
 
 import containerStyle from "./MyPageContainer.module.css";
+import getMemberInfo from "api/member/getMemberInfo";
+import { IMemberInfoDetails } from "interface/member/IMemberInfoDetails";
+import useErrorHandler from "hooks/error/useErrorHandler";
+import secureLocalStorage from "react-secure-storage";
 
 function MyPageContainer() {
+  const [memberInfo, setMemberInfo] = useState<IMemberInfoDetails>();
+  const { navigateOnError } = useErrorHandler();
+  const fetchMemberInfo = async () => {
+    const result: IMemberInfoDetails | null = await getMemberInfo();
+    if (result) setMemberInfo(result);
+  };
+  useEffect(() => {
+    if (secureLocalStorage.getItem("accessToken")) {
+      fetchMemberInfo();
+    } else {
+      navigateOnError({ errorType: "AUTH", path: "/login" });
+    }
+    //eslint-disable-next-line
+  }, []);
+  if (!memberInfo) return <div></div>;
   return (
     <div className={containerStyle.wrapper}>
       <div className={containerStyle.profileWrapper}>
-        <ProfileCard />
+        <ProfileCard memberInfo={memberInfo} />
       </div>
       <div className={containerStyle.userHistoryWrapper}>
         <UserHistoryContainer />
