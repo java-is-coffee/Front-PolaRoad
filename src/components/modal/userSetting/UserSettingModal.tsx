6b04e168dfc4de85bc965540cb45159d5a349159
koreaygj <ygj1828@gmail.com>
2024-04-04 13:48:18 +0900

    Feat : PO-20 프로필 카드 수정 모달 골조 구현

---
+++ src/components/modal/userSetting/UserSettingModal.tsx
@@ -0,0 +1,39 @@
+import { useModal } from "hooks/modal/ModalProvider";
+import modalStyles from "./UserSettingModal.module.css";
+import ModalOption from "enum/modalOptionTypes";
+import secureLocalStorage from "react-secure-storage";
+
+const UserSettingModal = () => {
+  const { closeModal } = useModal();
+
+  const handleCancel = () => {
+    closeModal(ModalOption.USER_SETTING);
+  };
+
+  const handleLogout = () => {
+    secureLocalStorage.clear();
+    window.location.href = "/";
+    handleCancel();
+  };
+
+  return (
+    <div className={modalStyles.backdrop} onClick={handleCancel}>
+      <div
+        className={modalStyles.modalContents}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <button>설정 및 개인정보</button>
+        <hr className={modalStyles.divider} />
+        <button>비밀번호 재설정</button>
+        <hr className={modalStyles.divider} />
+        <button className={modalStyles.warningOption} onClick={handleLogout}>
+          로그아웃
+        </button>
+        <hr className={modalStyles.divider} />
+        <button onClick={handleCancel}>취소</button>
+      </div>
+    </div>
+  );
+};
+
+export default UserSettingModal;
