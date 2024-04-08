99d4c27de2946dd77aa1f9027850ae0b801dc339
koreaygj <ygj1828@gmail.com>
2024-04-08 13:51:30 +0900

    Refactor : PO-85 모달 등록 방식 페이지 마다 등록하는 방식으로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import Explore from "./routes/Explore";
@@ -6,49 +6,13 @@ import MyPage from "./routes/MyPage";
 import Login from "./containers/login/LoginContainer";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 import "./styles/globals.css";
 import Post from "routes/Post";
-import { ModalProvider } from "hooks/modal/ModalProvider";
 
 // 사용되는 모달 등록
-import { ModalProvider, useModal } from "hooks/modal/ModalProvider";
-import ModalOption from "enum/modalOptionTypes";
-import NewPostModal from "components/modal/newPost/NewPostModal";
-import WarningModal from "components/modal/warn/WarningModal";
-import EditProfileImgModal from "components/modal/profileImg/EditProfileImgModal";
-import PostPreviewModal from "components/modal/post/PostPreviewModal";
-import ShareModal from "components/modal/shareModal/ShareModal";
-import UserInfoModal from "components/modal/userSetting/userInfo/UserInfoModal";
-import PostOptionModal from "components/modal/option/PostOptionModal";
-import UserSettingModal from "components/modal/userSetting/UserSettingModal";
-import NewAlbumModal from "components/modal/album/newAlbum/NewAlbumModal";
+import { ModalProvider } from "hooks/modal/ModalProvider";
 
 function App() {
-  const { registerModal, closeModal } = useModal();
-  useEffect(() => {
-    registerModal(ModalOption.POST, <NewPostModal />);
-    registerModal(ModalOption.WARNING, <WarningModal />);
-    registerModal(ModalOption.EDIT_PROFILE_IMG, <EditProfileImgModal />);
-    registerModal(ModalOption.POST_PREVIEW, <PostPreviewModal />);
-    registerModal(ModalOption.SHARE, <ShareModal />);
-    registerModal(ModalOption.USER_SETTING, <UserSettingModal />);
-    registerModal(ModalOption.USER_INFO, <UserInfoModal />);
-    registerModal(ModalOption.POST_OPTION, <PostOptionModal />);
-    registerModal(ModalOption.ALBUM, <NewAlbumModal />);
-    return () => {
-      closeModal(ModalOption.POST);
-      closeModal(ModalOption.WARNING);
-      closeModal(ModalOption.EDIT_PROFILE_IMG);
-      closeModal(ModalOption.POST_PREVIEW);
-      closeModal(ModalOption.SHARE);
-      closeModal(ModalOption.USER_SETTING);
-      closeModal(ModalOption.USER_INFO);
-      closeModal(ModalOption.POST_OPTION);
-      closeModal(ModalOption.ALBUM);
-    };
-    // eslint-disable-next-line
-  }, []);
   return (
     <div className="App">
       <Router>
--- src/routes/Explore.tsx
+++ src/routes/Explore.tsx
@@ -2,11 +2,23 @@ import { useEffect } from "react";
 import WebHeader from "../components/header/web/WebHeader";
 import MobileHeader from "../components/header/mobile/MobileHeader";
 import ExploreContainer from "../containers/explore/ExploreContainer";
-import { useModal } from "../hooks/modal/ModalProvider";
-import ModalOption from "../enum/modalOptionTypes";
 import SearchDropdown from "../components/dropDown/search/SearchDropdown";
 import { useMediaQuery } from "@mui/material";
 import BottomNavigator from "components/bottom/BottomNavigator";
+
+// 사용되는 모달 등록
+import { useModal } from "hooks/modal/ModalProvider";
+import ModalOption from "enum/modalOptionTypes";
+import NewPostModal from "components/modal/newPost/NewPostModal";
+import WarningModal from "components/modal/warn/WarningModal";
+import EditProfileImgModal from "components/modal/profileImg/EditProfileImgModal";
+import PostPreviewModal from "components/modal/post/PostPreviewModal";
+import ShareModal from "components/modal/shareModal/ShareModal";
+import UserInfoModal from "components/modal/userSetting/userInfo/UserInfoModal";
+import PostOptionModal from "components/modal/option/PostOptionModal";
+import UserSettingModal from "components/modal/userSetting/UserSettingModal";
+import NewAlbumModal from "components/modal/album/newAlbum/NewAlbumModal";
+import NewWishListModal from "components/modal/wish/newWishList/NewWishListModal";
 
 function Explore() {
   const { registerModal, closeModal } = useModal();
@@ -17,6 +29,16 @@ function Explore() {
   // 4. 모바일 : ~ 480px
   useEffect(() => {
     registerModal(ModalOption.SEARCH, <SearchDropdown />);
+    registerModal(ModalOption.POST, <NewPostModal />);
+    registerModal(ModalOption.WARNING, <WarningModal />);
+    registerModal(ModalOption.WISH, <NewWishListModal />);
+    registerModal(ModalOption.EDIT_PROFILE_IMG, <EditProfileImgModal />);
+    registerModal(ModalOption.POST_PREVIEW, <PostPreviewModal />);
+    registerModal(ModalOption.SHARE, <ShareModal />);
+    registerModal(ModalOption.USER_SETTING, <UserSettingModal />);
+    registerModal(ModalOption.USER_INFO, <UserInfoModal />);
+    registerModal(ModalOption.POST_OPTION, <PostOptionModal />);
+    registerModal(ModalOption.ALBUM, <NewAlbumModal />);
     return () => closeModal(ModalOption.SEARCH);
     // eslint-disable-next-line
   }, []);
