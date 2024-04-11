c947772cce49554d472b37b7c841025a0702f8e1
koreaygj <ygj1828@gmail.com>
2024-04-11 09:59:46 +0900

    Feat : PO-18 map 페이지 라우터 설정

---
+++ src/containers/map/MapPageContainer.tsx
@@ -0,0 +1,5 @@
+const MapPageContainer = () => {
+  return <div></div>;
+};
+
+export default MapPageContainer;
---
+++ src/routes/MapPage.tsx
@@ -0,0 +1,13 @@
+import Header from "components/header/web/WebHeader";
+import MapPageContainer from "containers/map/MapPageContainer";
+
+function MapPage() {
+  return (
+    <div>
+      <Header />
+      <MapPageContainer />
+    </div>
+  );
+}
+
+export default MapPage;
