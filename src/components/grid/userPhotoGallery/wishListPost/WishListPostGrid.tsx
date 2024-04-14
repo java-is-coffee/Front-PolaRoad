8be7606b22eac9803c4f25afe5ba01a66c191812
koreaygj <ygj1828@gmail.com>
2024-04-14 20:57:55 +0900

    Feat : PO-92 포스트 위시리스트에서 카드 그리드 형식으로 랜더링 구현

--- src/components/grid/userPhotoGallery/wishListPost/WishListPostGrid.tsx
+++ src/components/grid/userPhotoGallery/wishListPost/WishListPostGrid.tsx
@@ -1,8 +1,10 @@
+import WishListPostCard from "components/card/wishList/wishListPostCard/WishListPostCard";
 import gridStyles from "./WishListPostGrid.module.css";
 import getWishListDetails from "api/wishlist/getWishListDetails";
 import ModalOption from "enum/modalOptionTypes";
 import { useModal } from "hooks/modal/ModalProvider";
-import { useEffect } from "react";
+import { IWishListPost } from "interface/wish/IWishListPost";
+import { useEffect, useRef, useState } from "react";
 
 interface WishListPostGridProps {
   wishListId: number;
@@ -14,16 +16,22 @@ const WishListPostGrid = ({
   wishListName,
 }: WishListPostGridProps) => {
   const { openModal } = useModal();
+  const [wishList, setWishList] = useState<IWishListPost[]>([]);
+  const [hasNext, setHasNext] = useState<boolean>(true);
+  const lastPost = useRef(null); // 감시할 요소의 ref
   useEffect(
     () => {
       const fetchWishList = async () => {
         const data = await getWishListDetails(wishListId, 1, 12);
-        console.log(data);
+        if (data) {
+          setWishList(data.posts);
+          setHasNext(data.hasNext);
+        }
       };
       fetchWishList();
     },
     // eslint-disable-next-line
-    []
+    [wishListId, wishListName]
   );
 
   const handleDeleteWishList = () => {
@@ -44,6 +52,17 @@ const WishListPostGrid = ({
         >
           위시리스트 삭제
         </span>
+      </div>
+      <div className={gridStyles.gridGallery}>
+        {wishList.map((post, index) => (
+          <WishListPostCard
+            key={index}
+            postId={post.postId}
+            thumbnailImg={post.thumbnailImage}
+            title={post.title}
+          />
+        ))}
+        <div ref={lastPost} />
       </div>
     </div>
   );
